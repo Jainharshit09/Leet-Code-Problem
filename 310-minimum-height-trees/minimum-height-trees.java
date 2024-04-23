fa91a884f2596f9ede9f78ef45761d92ebf84698
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-04-23 22:45:20 +0530

    Time: 21 ms (77.52%) | Memory: 56 MB (64.31%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,40 +1,41 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
        ArrayList <Integer> ans=new ArrayList<>();
-        if (n == 1)
-            return Collections.singletonList(0);
-       int [] indgree=new int[n];
+       if(n==1){
+        ans.add(0);
+        return ans;
+       }
+       int [] indegree=new int[n];
        Map<Integer,ArrayList<Integer>>adj =new HashMap<>();
-       for(int edge[]:edges){
-        int u = edge[0];
-        int v = edge[1];
-        indgree[u]++;
-        indgree[v]++;
-         adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
-         adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
-       }
-       Queue <Integer> q= new LinkedList<>();
-       for(int i=0;i<n;i++){
-        if(indgree[i]==1){
-            q.add(i);
-        }
-       }
-       while(n>2){
-        int size=q.size();
-        n -= size;
-        while(size-->0){
-            int u=q.poll();
-            for(int v: adj.get(u)){
-                indgree[v]--;
-                if(indgree[v]==1){
-                    q.add(v);
+       for(int edge[] : edges){
+        int u=edge[0];
+        int v=edge[1];
+        indegree[u]++;
+        indegree[v]++;
+        adj.computeIfAbsent(u, k-> new ArrayList<>()).add(v);
+         adj.computeIfAbsent(v, k-> new ArrayList<>()).add(u);}
+         Queue<Integer> q=new LinkedList<>();
+         for(int i=0;i<n;i++){
+            if(indegree[i]==1){
+                q.add(i);
+            }
+         }
+         while(n>2){
+            int size=q.size();
+            n-=size;
+            while(size-->0){
+                int u=q.poll();
+                for(int v : adj.get(u)){
+                    indegree[v]--;
+                    if(indegree[v]==1){
+                        q.add(v);
+                    }
                 }
             }
-        }
-       }
-       while(!q.isEmpty()){
-        ans.add(q.poll());
-       }
-       return ans;
-   }
+         }
+         while(!q.isEmpty()){
+            ans.add(q.poll());
+         }
+         return ans;
+    }
 } 
