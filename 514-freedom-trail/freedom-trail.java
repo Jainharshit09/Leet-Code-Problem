164a3a79105d4a179ef5c0bc28b9c9b832c5c6d9
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-04-28 05:04:46 +0530

    Time: 151 ms (17.47%) | Memory: 44.6 MB (71.62%) - LeetSync

--- 514-freedom-trail/freedom-trail.java
+++ 514-freedom-trail/freedom-trail.java
@@ -1,25 +1,34 @@
 class Solution {
-    public int findRotateSteps(String ring, String key) {
-          char[] r=ring.toCharArray();
-        List<Integer>[] p=new List[26];
-        for(int i=0;i<r.length;i++) {
-            int c=r[i]-'a';
-            List<Integer> l=p[c];
-            if(l==null) p[c]=l=new ArrayList<>();
-            l.add(i);
-        }
-        return helper(0,0,p,key.toCharArray(),ring,new int[key.length()][r.length]);
+    private int countSteps(int ringIndex, int i, int n) {
+        int anticlockwise = Math.abs(i - ringIndex);
+        int clockwise = n - anticlockwise;
+        return Math.min(clockwise, anticlockwise);
     }
-    int helper(int in, int pos, List<Integer>[] p, char[] k, String r, int[][] memo) {
-        if(in==k.length) return 0;
-        if(memo[in][pos]>0) return memo[in][pos]-1;
-        int min=Integer.MAX_VALUE;
-        for(int i: p[k[in]-'a']) {
-            int m;
-            if(i>=pos) m=Math.min(i-pos,pos+r.length()-i);
-            else m=Math.min(pos-i,i+r.length()-pos);
-            min=Math.min(min,m+helper(in+1,i,p,k,r,memo));
+
+    public int findRotateSteps(String ring, String key) {
+        int[][] dp = new int[ring.length()][key.length() + 1];
+
+        // Initialize the base case
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return (memo[in][pos]=min+2)-1;
+        for (int ringIndex = 0; ringIndex < ring.length(); ringIndex++) {
+            dp[ringIndex][key.length()] = 0;
+        }
+
+        for (int keyIndex = key.length() - 1; keyIndex >= 0; keyIndex--) {
+            for (int ringIndex = 0; ringIndex < ring.length(); ringIndex++) {
+                int result = Integer.MAX_VALUE;
+                char keyChar = key.charAt(keyIndex);
+                for (int i = 0; i < ring.length(); i++) {
+                    if (ring.charAt(i) == keyChar) {
+                        int totalSteps = countSteps(ringIndex, i, ring.length()) + 1 + dp[i][keyIndex + 1];
+                        result = Math.min(result, totalSteps);
+                    }
+                }
+                dp[ringIndex][keyIndex] = result;
+            }
+        }
+        return dp[0][0];
     }
 }
