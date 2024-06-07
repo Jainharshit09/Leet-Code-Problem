4de8879889c5a6d4f02558c45e8e356ef622e075
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-06-08 00:35:47 +0530

    Time: 2 ms (85.59%) | Memory: 45.1 MB (80.86%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,49 +1,45 @@
 class Solution {
-    int n, m;
-    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    public void dfs(int i, int j, char[][] board, boolean[][] vis) {
-        vis[i][j] = true;
-        for (int[] dir : directions) {
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-            if (new_i >= 0 && new_i < n && new_j >= 0 && new_j < m && !vis[new_i][new_j] && board[new_i][new_j] == 'O') {
-                dfs(new_i, new_j, board, vis);
+    int n,m;
+    boolean vis[][];
+    int direction[][]={{0,1},{1,0},{0,-1},{-1,0}};
+    public void dfs(int i,int j,char board[][]){
+        vis[i][j]=true;
+        for(int[] dir : direction){
+            int new_i=i+dir[0];
+            int new_j=j+dir[1];
+            if(new_i>=0 &&  new_i<n && new_j>=0 && new_j<m &&!vis[new_i][new_j]&& board[new_i][new_j]=='O'){
+                dfs(new_i,new_j,board);
             }
         }
     }
-
     public void solve(char[][] board) {
-        if (board == null || board.length == 0) return;
-        n = board.length;
-        m = board[0].length;
-        
-        // Initialize visited array
-        boolean[][] vis = new boolean[n][m];
-
-        // Traverse the boundaries
-        for (int j = 0; j < m; j++) {
-            if (board[0][j] == 'O' && !vis[0][j]) {
-                dfs(0, j, board, vis);
+        n=board.length;
+        m=board[0].length;
+        // 1 st row
+        vis = new boolean[n][m];
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O' && !vis[0][j]){
+                dfs(0,j,board);
             }
-            if (board[n - 1][j] == 'O' && !vis[n - 1][j]) {
-                dfs(n - 1, j, board, vis);
+            //last row
+             if(board[n-1][j]=='O' && !vis[n - 1][j]){
+                dfs(n-1,j,board);
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (board[i][0] == 'O' && !vis[i][0]) {
-                dfs(i, 0, board, vis);
+        // 1st col
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O' && !vis[i][0]){
+                dfs(i,0,board);
             }
-            if (board[i][m - 1] == 'O' && !vis[i][m - 1]) {
-                dfs(i, m - 1, board, vis);
+            // col
+             if(board[i][m-1]=='O' && !vis[i][m - 1]){
+                dfs(i,m-1,board);
             }
         }
-
-        // Flip captured 'O' to 'X' within the surrounded regions
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 'O' && !vis[i][j]) {
-                    board[i][j] = 'X';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O'&& !vis[i][j]){
+                    board[i][j]='X';
                 }
             }
         }
