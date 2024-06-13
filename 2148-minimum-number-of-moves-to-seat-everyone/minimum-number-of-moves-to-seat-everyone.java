1f5723695676588df7a03c943d6121393aef582d
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-06-14 00:42:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (14.32%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.java
@@ -1,11 +1,32 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        int ans=0;
-        for(int i=0;i<seats.length;i++){
-            ans+=Math.abs(students[i]-seats[i]);
+       int st[]=new int[101];
+       int sa[]=new int[101];
+       for(int i=0;i<seats.length;i++){
+        int k=seats[i];
+        sa[k]=sa[k]+1;
+        int j=students[i];
+        st[j]=st[j]+1;
+       }
+       int ans=0;
+       int i=0;
+       int j=0;
+       while(i<sa.length && j<st.length){
+        while(i<sa.length && sa[i]==0){
+            i++;
         }
-        return ans;
-    }
+        while(j<st.length && st[j]==0){
+            j++;
+        }
+        if (i >= sa.length || j >= st.length) {
+                break;
+            }
+
+        int moves=Math.min(sa[i],st[j]);
+        ans+=moves * Math.abs(i - j);
+        sa[i]-=moves;
+        st[j]-=moves;
+     }
+     return ans;
+  }
 }
