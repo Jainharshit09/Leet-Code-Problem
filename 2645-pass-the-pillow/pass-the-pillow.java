ba77c4243a40d1b8bb4482ae69b21d195b7bfaaa
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-07-07 00:52:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (46.33%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -1,16 +1,25 @@
 class Solution {
     public int passThePillow(int n, int time) {
-        int idx=1;
-        int dir=1;
-        while(time>0){
-            if(idx+dir>=1 && idx+dir<=n){
-                time--;
-                idx+=dir;
-            }
-            else{
-                dir*=-1;
-            }
+        // int idx=1;
+        // int dir=1;
+        // while(time>0){
+        //     if(idx+dir>=1 && idx+dir<=n){
+        //         time--;
+        //         idx+=dir;
+        //     }
+        //     else{
+        //         dir*=-1;
+        //     }
+        // }
+        // return idx;
+
+        int full=time/(n-1);
+        int remaning=time%(n-1);
+        if(full%2!=0){
+            return n-remaning;
         }
-        return idx;
+        else{
+           return remaning+1;
+        }
     }
 }
