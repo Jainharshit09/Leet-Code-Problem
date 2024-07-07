5c2b5eb75ecb22e790fa1470abece3fbfac1cb53
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-07-08 01:00:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (28.61%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans=numBottles;
+        while(numBottles>=numExchange){
+         int divides=numBottles/numExchange;
+        int modulo=numBottles%numExchange;
+        ans+=divides;
+        numBottles=divides+modulo;
+        }
+        return ans;
+    }
+}
