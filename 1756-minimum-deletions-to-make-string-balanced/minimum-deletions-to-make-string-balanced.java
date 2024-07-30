d300fc693e2ff42817d7a467e1f6047b9612faaf
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-07-31 01:01:35 +0530

    Time: 24 ms (54.60%) | Memory: 45.3 MB (77.16%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,19 +1,19 @@
-import java.util.Stack;
-
 class Solution {
     public int minimumDeletions(String st) {
-        Stack<Character> s = new Stack<>();
-        int count = 0;
+        int countB = 0;
+        int deletions = 0;
 
         for (int i = 0; i < st.length(); i++) {
-            if (!s.isEmpty() && s.peek() == 'b' && st.charAt(i) == 'a') {
-                s.pop();
-                count++;
-            }
-            else{
-                s.push(st.charAt(i));
+            if (st.charAt(i) == 'b') {
+                countB++;
+            } else if (st.charAt(i) == 'a') {
+                if (countB > 0) {
+                    deletions++;
+                    countB--;
+                }
             }
         }
-        return count;
+
+        return deletions;
     }
 }
