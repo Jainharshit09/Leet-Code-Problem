ed25bf41590c13adf3440660eb7ea83af55f7990
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-08-29 23:34:27 +0530

    Time: 33 ms (29.70%) | Memory: 45.4 MB (9.35%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -1,24 +1,49 @@
 class Solution {
-    public void dfs(int  stones[][],int index,boolean vis[]){
-        vis[index]=true;
-        for(int i=0;i<stones.length;i++){
-            int r=stones[index][0];
-            int c=stones[index][1];
-            if(!vis[i] && (stones[i][0]==r || stones[i][1]==c)){
-                dfs(stones,i,vis);
-           }
+    int n;
+    int parent[];
+    int rank[];
+    public int find(int i){
+        if(parent[i]!=i){
+            parent[i]=find(parent[i]);
+        }
+        return parent[i];
+    }
+    public void union(int i,int j){
+        int rooti=find(i);
+        int rootj=find(j);
+        if(rooti!=rootj){
+            if(rank[rooti]>rank[rootj]){
+                parent[rootj]=rooti;
+            }
+            else if(rank[rootj]>rank[rooti]){
+                parent[rooti]=rootj;
+            }
+            else{
+                parent[rootj]=parent[rooti];
+                rank[rooti]++;
+            }
         }
     }
     public int removeStones(int[][] stones) {
-        int n=stones.length;
-        boolean vis[]=new boolean[n];
+         n=stones.length;
+        parent=new int[n];
+        rank=new int [n];
         int group=0;
         for(int i=0;i<n;i++){
-            if(vis[i]){
-                continue;
+            parent[i]=i;
+            rank[i]=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    union(i,j);
+                }
             }
-                dfs(stones,i,vis);
-                group++; 
+        }
+        for(int i=0;i<n;i++){
+            if(parent[i]==i){
+                group++;
+            }
         }
         return n-group;
     }
