452ac84f59d062314cdc6f5000974dfc83c415cd
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-08-31 12:58:59 +0530

    Time: 41 ms (29.47%) | Memory: 58.8 MB (9.89%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,26 +1,56 @@
+import java.util.*;
+
+class Pair implements Comparable<Pair> {
+    double wt;
+    int v;
+
+    public Pair(double wt, int v) {
+        this.wt = wt;
+        this.v = v;
+    }
+
+    @Override
+    public int compareTo(Pair other) {
+        return Double.compare(other.wt, this.wt);
+    }
+}
+
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double result[]=new double[n];
-        result[start_node]=1.0;
-        for(int i=0;i<n-1;i++){
-            boolean update=false;
-            for(int j=0;j<edges.length;j++){
-                int u=edges[j][0];
-                int v=edges[j][1];
-                double wt=succProb[j];
-                if(result[u]*wt>result[v]){
-                    update=true;
-                    result[v]=result[u]*wt;
-                }
-                if(result[v]*wt>result[u]){
-                    update=true;
-                    result[u]=result[v]*wt;
-                }
+        Map<Integer, List<Pair>> graph = new HashMap<>();
+
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double prob = succProb[i];
+            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair(prob, v));
+            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair(prob, u));
+        }
+
+        double[] result = new double[n];
+        result[start_node] = 1.0;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(1.0, start_node));
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.poll();
+            double curProb = curr.wt;
+            int curNode = curr.v;
+
+            if (curNode == end_node) {
+                return curProb;
             }
-            if(!update){
-               break;
+
+            for (Pair nxt : graph.getOrDefault(curNode, new ArrayList<>())) {
+                int nxtNode = nxt.v;
+                double pathProb = nxt.wt;
+                if (curProb * pathProb > result[nxtNode]) {
+                    result[nxtNode] = curProb * pathProb;
+                    pq.add(new Pair(result[nxtNode], nxtNode));
+                }
             }
         }
-        return result[end_node];
+        return 0.0;
     }
 }
