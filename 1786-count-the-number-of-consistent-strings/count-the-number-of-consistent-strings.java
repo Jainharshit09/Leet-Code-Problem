d40bba3342e2f134a16c2a8d7db1286ca39c21ba
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-09-12 18:35:48 +0530

    Time: 6 ms (88.76%) | Memory: 45.1 MB (75.00%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -1,22 +1,22 @@
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        Set<Character> set=new HashSet<>();
-        for(int i=0;i<allowed.length();i++){
-            set.add(allowed.charAt(i));
+        int mask=0;
+        for(char ch:allowed.toCharArray()){
+            mask|=(1<< (ch-'a'));
         }
         int count=0;
-        for(int i=0;i<words.length;i++){
-            String ch=words[i];
-            int temp=0;
-            for(int k=0;k<ch.length();k++){
-                char c=ch.charAt(k);
-                if(!set.contains(c)){
-                    temp=0;
+        for(String word:words){
+            boolean persent=true;
+             for (int i = 0; i < word.length(); i++){
+                char ch=word.charAt(i);
+                if(((mask >> (ch-'a'))& 1)==0){
+                    persent=false;
                     break;
                 }
-                temp=1;
             }
-            count+=temp;
+            if(persent){
+                count++;
+            }
         }
         return count;
     }
