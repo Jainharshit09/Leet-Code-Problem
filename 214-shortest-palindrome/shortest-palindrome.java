9917d61302f7c70fe2cdeb5d24439daea862535a
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-09-20 18:58:11 +0530

    Time: 540 ms (21.06%) | Memory: 44.3 MB (96.89%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
-    public boolean isPalindrome(String s, int start, int end){
-        while (start<end) {
-            if (s.charAt(start)!=s.charAt(end)) {
+    public boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
                 return false;
             }
             start++;
@@ -9,19 +9,24 @@ class Solution {
         }
         return true;
     }
-    public String solve(String s,int end){
-        if (end<0) {
+
+    public String solve(String s, int end) {
+        if (end < 0) {
             return "";
         }
-        if(isPalindrome(s,0,end)){
-            String toAdd = s.substring(end+1);
-            String reversedToAdd=new StringBuilder(toAdd).reverse().toString();
-            return reversedToAdd+s;
+        while (end >= 0) {
+            if (isPalindrome(s, 0, end)) {
+                String toAdd = s.substring(end + 1);
+                String reversedToAdd = new StringBuilder(toAdd).reverse().toString();
+                return reversedToAdd + s;
+            }
+            end--;
         }
-        return solve(s,end-1);
+        return s;
     }
+
     public String shortestPalindrome(String s) {
-        if(s==null || s.isEmpty()){
+        if (s == null || s.isEmpty()) {
             return s;
         }
         return solve(s, s.length() - 1);
