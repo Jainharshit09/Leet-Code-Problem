36bfdf37a58760bf5293825918f1f9b458a31812
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2024-10-13 20:46:40 +0530

    Time: 34 ms (50.87%) | Memory: 49.1 MB (23.00%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
         int k=nums.size();
-        int[]ans=new int[k];
+        int maxEl = Integer.MIN_VALUE;
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for(int i=0;i<k;i++){
+            pq.add(new int[]{nums.get(i).get(0), i, 0});
+            maxEl=Math.max(maxEl, nums.get(i).get(0));
+        }
         int [] result= {-1000000,1000000};
-        while(true){
-           int minEl = Integer.MAX_VALUE;
-           int maxEl = Integer.MIN_VALUE;
-           int minElListIdx=0;
-           for(int i=0;i<k;i++){
-            int listIdx=i;
-            int elIdx = ans[i];
-            int element=nums.get(listIdx).get(elIdx);
-            if (element < minEl){
-                minEl=element;
-                minElListIdx=listIdx;
+        while(!pq.isEmpty()){
+            int curr[]=pq.poll();
+            int minEl=curr[0];
+            int listIdx=curr[1];
+            int idx=curr[2];
+            if (maxEl - minEl < result[1] - result[0]) {
+                result[0] = minEl;
+                result[1] = maxEl;
             }
-            maxEl=Math.max(maxEl,element);
-           }
-           if(maxEl-minEl<result[1]-result[0]){
-            result[0]=minEl;
-            result[1]=maxEl;
-           }
-           int nextIndex = ans[minElListIdx] + 1;
-           if (nextIndex >= nums.get(minElListIdx).size()){
-            break;
-           }
-           ans[minElListIdx]=nextIndex;
+            if(idx+1<nums.get(listIdx).size()){
+                int nextElement=nums.get(listIdx).get(idx+1);
+                pq.add(new int[]{nextElement,listIdx,idx+1});
+                maxEl=Math.max(maxEl,nextElement);
+            }
+            else{
+                break;
+            }
         }
         return result;
     }
