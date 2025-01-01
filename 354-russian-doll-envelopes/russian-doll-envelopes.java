725d9e1d97c8fdbc46f752fd1c14b232086c21db
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-01-02 03:08:46 +0530

    Time: 59 ms (10.47%) | Memory: 101.6 MB (83.86%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -1,31 +1,15 @@
+
 class Solution {
-    public int binarySearch(int[] dp, int val){
-        int lo=0,hi=dp.length-1,res=0;
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            if(dp[mid]<val){
-                res=mid;
-                lo=mid+1;
-            }else{
-                hi=mid-1;
-            }
-        }
-        return res+1;
-    }
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes,(a,b)->a[0]==b[0]?b[1]-a[1]:a[0]-b[0]);
-        int[] LIS=new int[envelopes.length+1];
-        Arrays.fill(LIS,Integer.MAX_VALUE);
-        LIS[0]=Integer.MIN_VALUE;
-        int ans=0;
-        for(int i=0;i<envelopes.length;i++){
-            int val=envelopes[i][1];
-            int insertIndex=binarySearch(LIS,val);
-            ans=Math.max(ans,insertIndex);
-            if(LIS[insertIndex]>=val){
-                LIS[insertIndex]=val;
-            }
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        ArrayList<Integer> lis = new ArrayList<>();
+        for (int[] envelope : envelopes) {
+            int height = envelope[1];
+            int pos = Collections.binarySearch(lis, height);
+            if (pos < 0) pos = -(pos + 1);
+            if (pos == lis.size()) lis.add(height);
+            else lis.set(pos, height);
         }
-        return ans;
+        return lis.size();
     }
 }
