f35fb31f8ad3a649aca4db017a4feb219a3fd26d
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-01-10 16:35:34 +0530

    Time: 29 ms (15.12%) | Memory: 54.2 MB (86.05%) - LeetSync

--- 952-word-subsets/word-subsets.java
+++ 952-word-subsets/word-subsets.java
@@ -1,14 +1,17 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
         List<String> ans = new ArrayList<>();
-        int[] maxFreq = new int[26];
+        Map<Character, Integer> maxFreqMap = new HashMap<>();
         for (String word : words2) {
             int[] freq = new int[26];
             for (char c : word.toCharArray()) {
                 freq[c - 'a']++;
             }
             for (int i = 0; i < 26; i++) {
-                maxFreq[i] = Math.max(maxFreq[i], freq[i]);
+                char key = (char) ('a' + i);
+                if (freq[i] > 0) {
+                    maxFreqMap.put(key, Math.max(maxFreqMap.getOrDefault(key, 0), freq[i]));
+                }
             }
         }
         for (String word : words1) {
@@ -17,8 +20,10 @@ class Solution {
                 freq[c - 'a']++;
             }
             boolean isUniversal = true;
-            for (int i = 0; i < 26; i++) {
-                if (freq[i] < maxFreq[i]) {
+            for (Map.Entry<Character, Integer> entry : maxFreqMap.entrySet()) {
+                char key = entry.getKey();
+                int requiredFreq = entry.getValue();
+                if (freq[key - 'a'] < requiredFreq) {
                     isUniversal = false;
                     break;
                 }
@@ -30,3 +35,4 @@ class Solution {
         return ans;
     }
 }
+
