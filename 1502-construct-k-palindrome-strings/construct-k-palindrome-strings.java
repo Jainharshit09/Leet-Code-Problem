e7eaf7110c0bb745aa943ea2181ca3f26b0e1e32
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-01-12 00:30:06 +0530

    Time: 3 ms (100.00%) | Memory: 45.4 MB (61.15%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -7,20 +7,10 @@ class Solution {
         if(n==k){
             return true;
         }
-        int arr[]=new int [26];
-        for(int i=0;i<n;i++){
-            int idx=s.charAt(i)-'a';
-            arr[idx]++;
-        }
         int odd_count=0;
-        for(int i=0;i<26;i++){
-            if(arr[i]%2==1){
-                odd_count++;
-            }
+        for(char ch:s.toCharArray()){
+            odd_count^=1<<(ch-'a');
         }
-        if(odd_count>k){
-            return false;
-        }
-        return true;
+         return Integer.bitCount(odd_count) <= k;
     }
 }
