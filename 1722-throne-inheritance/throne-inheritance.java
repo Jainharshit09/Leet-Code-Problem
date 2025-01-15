e34bd641f09097bc2a51c34bf1f80531136593d5
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-01-16 01:31:59 +0530

    Time: 277 ms (29.66%) | Memory: 105.3 MB (24.58%) - LeetSync

---
+++ 1722-throne-inheritance/throne-inheritance.java
@@ -0,0 +1,42 @@
+class ThroneInheritance {
+    HashMap<String,List<String>>map;
+    Set<String>set;
+    String root;
+    public ThroneInheritance(String kingName) {
+        map=new HashMap<>();
+        set=new HashSet<>();
+        map.putIfAbsent(kingName, new ArrayList<>());
+        root=kingName;
+    }
+    
+    public void birth(String parentName, String childName) {
+        map.get(parentName).add(childName);
+         map.putIfAbsent(childName,new ArrayList<>());
+    }
+    
+    public void death(String name) {
+        set.add(name);
+    }
+    
+    public List<String> getInheritanceOrder() {
+        List<String>list=new ArrayList<>();
+        dfs(root,list);
+        return list;
+    }
+    public void dfs(String root,List<String>list){
+        if(!set.contains(root)){
+            list.add(root);
+        }
+        for(String child:map.get(root)){
+            dfs(child,list);
+        }
+    }
+}
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance obj = new ThroneInheritance(kingName);
+ * obj.birth(parentName,childName);
+ * obj.death(name);
+ * List<String> param_3 = obj.getInheritanceOrder();
+ */
