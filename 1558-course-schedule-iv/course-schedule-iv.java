5b635675564eac9771309355a2a157a0bbae88af
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-01-27 22:14:29 +0530

    Time: 59 ms (33.97%) | Memory: 47.6 MB (77.35%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,31 +1,37 @@
 class Solution {
-    public boolean dfs(HashMap<Integer,List<Integer>> graph,int start,int end,boolean[] vis){
-        vis[start]=true;
-        if(start==end){
-            return true;
-        }
-         boolean isReachable = false;
-         for (int adjNode : graph.getOrDefault(start, new ArrayList<>())) {
-            if(!vis[adjNode]){
-                isReachable = isReachable || dfs(graph, adjNode, end, vis);
-            }
-         }
-         return isReachable;
-    }
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] preq, int [][] queries) {
-        List<Boolean> ans= new ArrayList<>();
         HashMap<Integer,List<Integer>>adjList=new HashMap<>();
+        int indegree[]=new int[numCourses];
         int n=preq.length;
         int Q = queries.length;
         for (int[] edge : preq) {
             adjList.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            indegree[edge[1]]++;
         }
-        for(int i=0;i<Q;i++){
-            int u=queries[i][0];
-            int v=queries[i][1];
-            boolean vis[]=new boolean[numCourses];
-            ans.add(dfs(adjList,u,v,vis));
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
         }
-        return ans;
+        Map<Integer,Set<Integer>>set=new HashMap<>();
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int ngbr:adjList.getOrDefault(node, new ArrayList<>())) {
+                set.computeIfAbsent(ngbr, k -> new HashSet<>()).add(node);
+                set.get(ngbr).addAll(set.getOrDefault(node, new HashSet<>()));
+                indegree[ngbr]--;
+                if( indegree[ngbr]==0){
+                    q.add(ngbr);
+                }
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int[] query : queries) {
+            int src = query[0];
+            int dest = query[1];
+            result.add(set.getOrDefault(dest, new HashSet<>()).contains(src));
+        }
+        return result;
     }
 }
