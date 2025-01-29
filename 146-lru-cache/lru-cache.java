e48887510d6ef864d7ad501552630ea3b47a4ffc
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-01-29 20:09:32 +0530

    Time: 43 ms (86.94%) | Memory: 111.5 MB (91.03%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,35 @@
+
+class LRUCache {
+    class LRUMap extends LinkedHashMap<Integer, Integer>{
+        private final int capacity;
+
+        LRUMap(int capacity) {
+            super(capacity, 0.75f, true);
+            this.capacity = capacity;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+            return size() > capacity;
+        }
+    }
+     LRUMap cache;
+    public LRUCache(int capacity) {
+        cache=new LRUMap(capacity);
+    }
+    
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
