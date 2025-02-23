296141b90698f34f43f305c4607b7b8c94b158b4
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-02-23 12:34:17 +0530

    Time: 1 ms (53.87%) | Memory: 43.6 MB (41.67%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-   public TreeNode solve(int prestart,int poststart,int preend,int [] preorder,int [] postorder){
+   public TreeNode solve(int prestart,int poststart,int preend,int [] preorder,int [] postorder,HashMap<Integer,Integer>map){
     if (prestart > preend) {
             return null;
         }
@@ -22,18 +22,19 @@ class Solution {
     if(prestart==preend){
         return root;
     }
-    int j=poststart;
     int nextNode = preorder[prestart + 1]; 
-    while(postorder[j]!=nextNode){
-        j++;
-    }
+    int j=map.get(nextNode);
     int num=j-poststart+1;
-    root.left=solve(prestart+1,poststart,prestart+num,preorder,postorder);
-    root.right=solve(prestart+num+1,j+1,preend,preorder,postorder);
+    root.left=solve(prestart+1,poststart,prestart+num,preorder,postorder,map);
+    root.right=solve(prestart+num+1,j+1,preend,preorder,postorder,map);
     return root;
    }
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
         int n=preorder.length;
-        return solve(0,0,n-1,preorder,postorder);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(postorder[i],i);
+        }
+        return solve(0,0,n-1,preorder,postorder,map);
     }
 }
