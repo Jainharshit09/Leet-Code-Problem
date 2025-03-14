38c7a1ef90c22fb7252179e84968c53c5a5678ca
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-03-14 16:31:59 +0530

    Time: 25 ms (73.66%) | Memory: 63.7 MB (7.93%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,46 +1,41 @@
 class Solution {
-    private boolean canDistr(int[] candies, int mid, long k) {
+        private boolean canDistr(int[] candies, int mid, long k) {
         int n = candies.length;
 
         for (int i = 0; i < n; i++) {
             k -= candies[i] / mid;
 
-            if (k <= 0) { // all children got mid candies
-                return true; // Early return
+            if (k <= 0) {
+                return true;
             }
         }
 
-        return k <= 0; // all children got the mid candies
+        return k <= 0;
     }
-
     public int maximumCandies(int[] candies, long k) {
-        int n = candies.length;
-
-        int maxC = 0;
-        long total = 0;
-        for (int i = 0; i < n; i++) {
-            total += candies[i];
-            maxC = Math.max(maxC, candies[i]);
+        int n=candies.length;
+        long total=0;
+        int max=0;
+        for(int i=0;i<n;i++){
+            total+=candies[i];
+            max=Math.max(max,candies[i]);
         }
-
-        if (total < k) {
+        if(total<k){
             return 0;
         }
-
-        int l = 1;
-        int r = maxC;
-        int result = 0;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-
-            if (canDistr(candies, mid, k)) {
-                result = mid;
+        int l=1;
+        int r=max;
+        int ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if (canDistr(candies, mid, k)){
+                ans=mid;
                 l = mid + 1;
-            } else {
-                r = mid - 1;
+            }
+            else {
+                r=mid-1;
             }
         }
-
-        return result;
+        return ans;
     }
 }
