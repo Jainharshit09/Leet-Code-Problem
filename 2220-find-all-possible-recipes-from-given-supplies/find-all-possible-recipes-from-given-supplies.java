1d23e6f90de24f69d6abc0c5335d0f623bb7dcdd
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-03-21 20:10:37 +0530

    Time: 32 ms (93.82%) | Memory: 46.1 MB (98.23%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,24 +1,39 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        int n = recipes.length;
-        boolean[] make = new boolean[n];
-        Set<String> set = new HashSet<>(Arrays.asList(supplies));
-        List<String> ans = new ArrayList<>();
-        int count = n;
-        while (count-- > 0) {
-            for (int i = 0; i < n; i++) {
-                if (make[i]) continue;
-                boolean canMake = true;
-                for (String ing : ingredients.get(i)) {
-                    if (!set.contains(ing)) {
-                        canMake = false;
-                        break;
-                    }
+        int n=recipes.length;
+        List<String>ans=new ArrayList<>();
+        int indegree[]=new int[n];
+        Set<String>set=new HashSet<>();
+        HashMap<String,ArrayList<Integer>>adj=new HashMap<>();
+        for(int i=0;i<supplies.length;i++){
+            set.add(supplies[i]);
+        }
+        for(int i=0;i<n;i++){
+            for(String ing:ingredients.get(i)){
+                if(!set.contains(ing)){
+                    adj.putIfAbsent(ing, new ArrayList<>());
+                    adj.get(ing).add(i);
+                    indegree[i]++;
                 }
-                if (canMake) {
-                    ans.add(recipes[i]);
-                    set.add(recipes[i]);
-                    make[i] = true;
+            }
+        }
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty())
+        {
+            int curr=q.poll();
+            String recipe=recipes[curr];
+            ans.add(recipe);
+            if(adj.containsKey(recipe)){
+                for(int idx : adj.get(recipe)){
+                    indegree[idx]--;
+                    if(indegree[idx]==0){
+                        q.add(idx);
+                    }
                 }
             }
         }
