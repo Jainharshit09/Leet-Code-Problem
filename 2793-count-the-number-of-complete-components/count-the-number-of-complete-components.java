ad15455ac339a0ae7263767fa9d6f9fd13bccf07
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-03-22 17:26:05 +0530

    Time: 16 ms (28.08%) | Memory: 44.9 MB (96.40%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean solve(HashMap<Integer,List<Integer>>adj,boolean vis[],int i){
+        int edge=0;
+        int vertex=0;
+        vis[i]=true;
+        Queue<Integer>q=new LinkedList<>();
+        q.add(i);
+        while(!q.isEmpty()){
+            int curr=q.poll();
+             vertex++;
+            if(adj.containsKey(curr)){
+                List<Integer>arr=adj.get(curr);
+                edge+=arr.size();
+                for (int ngbr : arr) {
+                    if (!vis[ngbr]) {
+                        vis[ngbr] = true;
+                        q.add(ngbr);
+                    }
+                }
+            }
+        }
+         return (vertex * (vertex- 1)) / 2 == edge/ 2;
+    }
+    public int countCompleteComponents(int n, int[][] edges) {
+         HashMap<Integer,List<Integer>>adj=new HashMap<>();
+         for (int i = 0; i < n; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        for(int edge[]:edges){
+            int u=edge[0];
+            int v=edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int ans=0;
+        boolean vis[]=new boolean[n];
+        for(int i=0;i<n;i++){
+            if(vis[i]!=true){
+                if(solve(adj,vis,i)){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
