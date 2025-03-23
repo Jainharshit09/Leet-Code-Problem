23f0e31cedb8a72a0735e7c4befa6c82210ebb16
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-03-23 08:03:50 +0530

    Time: 23 ms (5.80%) | Memory: 49.2 MB (67.91%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,51 +1,52 @@
-import java.util.*;
-
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        PriorityQueue<Pair> q = new PriorityQueue<>((x, y) -> Long.compare(x.a, y.a));
-        List<List<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int[] ar : roads) {
-            adj.get(ar[0]).add(new Pair(ar[1], ar[2])); // node wt
-            adj.get(ar[1]).add(new Pair(ar[0], ar[2]));
-        }
-        long[] dis = new long[n];
-        int[] ways = new int[n];
+        PriorityQueue<long[]>pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[1]));
+        HashMap<Integer,HashMap<Integer,Integer>>adj=new HashMap<>();
+        long dist[]=new long[n];
+        int ways[]=new int[n];
+        Arrays.fill(dist,Long.MAX_VALUE);
+        ways[0]=1;
+        dist[0]=0;
 
-        Arrays.fill(dis, Long.MAX_VALUE / 2);
-        dis[0] = 0;
-        ways[0] = 1;
-        q.add(new Pair(0, 0));
+        //create a graph with its next Node and wt
+        for(int i=0;i<n;i++){
+            adj.put(i,new HashMap<>());
+        }
 
-        while (!q.isEmpty()) {
-            long pd = q.peek().a;
-            long node = q.peek().b;
-            q.remove();
-            for (Pair i : adj.get((int) node)) {
-                long edgewt = i.b;
-                long adjnode = i.a;
-                if (edgewt + pd < dis[(int) adjnode]) {
-                    dis[(int) adjnode] = edgewt + pd;
-                    q.add(new Pair(dis[(int) adjnode], adjnode));
-                    ways[(int) adjnode] = ways[(int) node];
-                } else if (edgewt + pd == dis[(int) adjnode]) {
-                    ways[(int) adjnode] = (ways[(int) adjnode] + ways[(int) node]) % ((int) 1e9 + 7); // Corrected ways count
+        for(int i=0;i<roads.length;i++){
+            int u=roads[i][0];
+            int v=roads[i][1];
+            int wt=roads[i][2];
+            adj.get(u).put(v,wt);
+            adj.get(v).put(u,wt);
+        }
+
+        //starting node
+        pq.add(new long[]{0,0});
+
+
+        while(!pq.isEmpty()){
+            long curr[]=pq.poll();
+            int curr_node=(int)curr[0];
+            long curr_wt=curr[1];
+            for(Map.Entry<Integer, Integer> entry : adj.get(curr_node).entrySet()){
+                int nextNode=entry.getKey();
+                int edgeWt = entry.getValue();
+                long newDis=curr_wt+edgeWt;
+
+                // new dis is small to previous
+                if(newDis<dist[nextNode]){
+                    dist[nextNode]=newDis;
+                    pq.add(new long[]{nextNode,newDis});
+                    ways[nextNode]=ways[curr_node];
+                }
+
+                //eqaul distance between from both curr and previous so we can add that both
+                else if(newDis==dist[nextNode]){
+                    ways[nextNode]=(ways[curr_node]+ ways[nextNode])%((int) 1e9 + 7);
                 }
             }
         }
-
-        return ways[n - 1] % ((int) 1e9 + 7);
-    }
-}
-
-class Pair {
-    long a; // distance
-    long b; // node
-
-    Pair(long a, long b) {
-        this.a = a;
-        this.b = b;
+        return ways[n-1]%((int) 1e9 + 7);
     }
 }
