57a900c6dd1033bae39e7647a13a011f18d1f054
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-04-08 16:07:26 +0530

    Time: 28 ms (22.45%) | Memory: 44.7 MB (8.06%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,4 +1,6 @@
 class Solution {
+    //space com=2d array of 103 103 it is constant 
+    //time complexit is O(m^3) because for solve function n^2 and runinng one more loop for finding min 
     int dp[][]=new int[103][103];
     public int solve( ArrayList<Integer>list,int l,int r){
         if(r-l<2){
@@ -18,6 +20,7 @@ class Solution {
     public int minCost(int n, int[] cuts) {
         Arrays.sort(cuts);
         ArrayList<Integer>list=new ArrayList<>();
+        //in starting we added 0 and in last add n  beacuse it will starting and ending easy to calculate the cost and always that it will start from l+1 and end at r-1 not on r because it n value we need where we cut
         list.add(0);
         for(int i=0;i<cuts.length;i++){
             list.add(cuts[i]);
