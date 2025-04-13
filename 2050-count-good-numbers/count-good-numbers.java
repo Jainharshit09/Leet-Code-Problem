f023f7256686963ecb845e069b37cda4fb3d7435
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-04-13 13:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (37.30%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,16 +1,15 @@
 class Solution {
     private long powMod(long base, long exp, long mod) {
-        long result = 1;
-        base %= mod;
-        
-        while (exp > 0) {
-            if ((exp & 1) == 1) {
-                result = (result * base) % mod;
-            }
-            base = (base * base) % mod;
-            exp >>= 1;
-        }
-        return result;
+       if(exp==0){
+        return 1;
+       }
+       long result=1;
+       long half=powMod(base,exp/2,mod);
+       result=(half*half)%mod;
+       if(exp%2==1){
+        result=(result*base)%mod;
+       }
+       return result;
     }
     public int countGoodNumbers(long n) {
          long MOD = 1_000_000_007;
