66660e0ecfdccac2850c35cda11d32a8f32ce5f2
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-04-20 17:42:21 +0530

    Time: 3 ms (62.24%) | Memory: 73.9 MB (22.52%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.java
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maximumPossibleSize(int[] nums) {
         int n=nums.length-1;
-        Stack<Integer>stack=new Stack<>();
-        for(int i=n;i>=0;i--){
-            while(!stack.isEmpty() && stack.peek()<nums[i]){
-                stack.pop();
+        int ans=0;
+        int prev=Integer.MIN_VALUE;
+        for(int a:nums){
+            if(a>=prev){
+                ans++;
+                prev=a;
             }
-            stack.push(nums[i]);
         }
-        return stack.size();
+        return ans;
     }
 }
