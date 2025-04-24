9b9b977699761b5886f9570d1c9caf4ed0d5d2d4
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-04-25 01:24:43 +0530

    Time: 1 ms (73.96%) | Memory: 45.8 MB (46.46%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,22 +1,30 @@
+import java.util.*;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int row[]=new int[n];
-        int col[]=new int[m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
                 }
             }
         }
-      for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i]==1 || col[j]==1){
-                    matrix[i][j]=0;
-                }
+
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int row = pos[0];
+            int col = pos[1];
+
+            for (int j = 0; j < m; j++) {
+                matrix[row][j] = 0;
+            }
+
+            for (int i = 0; i < n; i++) {
+                matrix[i][col] = 0;
             }
         }
     }
