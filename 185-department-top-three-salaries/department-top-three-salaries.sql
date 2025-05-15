00ad0cb3c9e7b88057142bec10c038efa05e3680
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-05-16 00:32:30 +0530

    Time: 1570 ms (19.38%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -2,5 +2,7 @@
 select d.name as Department ,
     e.name as Employee ,
     e.salary as Salary
-from Employee e inner join Department d on e.departmentId=d.id where 3>
-(select count(distinct(e2.salary)) from Employee e2  where e2.salary>e.salary AND e.departmentId =e2.departmentId);
+from Employee e left join  Department d on e.departmentId=d.id 
+where 3>(
+    select count(distinct(e2.salary)) from Employee e2  
+    where e2.salary>e.salary AND e.departmentId =e2.departmentId);
