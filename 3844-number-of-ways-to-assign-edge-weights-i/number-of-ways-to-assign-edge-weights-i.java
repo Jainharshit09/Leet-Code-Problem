7daccd96410344f802651460a36930dcb02bc5ac
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-06-01 02:12:57 +0530

    Time: 193 ms (13.88%) | Memory: 116.1 MB (36.32%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    int MOD=1000000007;
+    public void dfs(int curr, HashMap<Integer,List<Integer>>adj,int parent,int depth,int maxDepth[]){
+        maxDepth[0]=Math.max(depth,maxDepth[0]);
+        for(int ngbr:adj.getOrDefault(curr,new ArrayList<>())){
+            if(parent!=ngbr){
+                dfs(ngbr,adj,curr,depth+1,maxDepth);
+            }
+        }
+    }
+    public int assignEdgeWeights(int[][] edges) {
+        HashMap<Integer,List<Integer>>adj=new HashMap<>();
+        int n=edges.length+1;
+        for(int i=1;i<=n;i++){
+            adj.put(i,new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int maxDepth[]=new int[1];
+        maxDepth[0]=0;
+        dfs(1,adj,-1,0,maxDepth);
+        int result=1;
+        for(int i=1;i<maxDepth[0];i++){
+            result=(result*2)%MOD;
+        }
+        return result;
+    }
+}
