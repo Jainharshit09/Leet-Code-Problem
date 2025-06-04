ffcbf27eb5c6507c634867b54d192ad1ad8292cf
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-06-04 19:35:42 +0530

    Time: 11 ms (57.41%) | Memory: 45.4 MB (68.52%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.java
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.java
@@ -1,4 +1,7 @@
 class Solution {
+    // rather than finding max then store and do for all index we can find the maxlength using n-(numFriends-1) then do for every index not on ma character find subtring word.substring(i,i+mini) the mini is that like abdc now ans dc the maxlength is 3 so not possible we can min(task,n-i) it will min length that possible and store in result and compare which one is greater store that and return the result
+
+// Time complexity:o(n*n) n for travese and n for substring
     public String answerString(String word, int numFriends) {
         if(numFriends==1){
             return word;
