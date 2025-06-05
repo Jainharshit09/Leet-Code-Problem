8ca7276f96458da64e6743cf3c11577bfb9536bf
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-06-06 02:29:09 +0530

    Time: 2 ms (96.88%) | Memory: 41.8 MB (93.53%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
@@ -1,39 +1,42 @@
 class Solution {
-    // create a grpah then do a basic dfs
-    // in dfs only need to find the min character 
-    public char dfs(HashMap<Character,List<Character>>adj,char curr_ch,boolean vis[]){
-        vis[curr_ch-'a']=true;
-        char ans=curr_ch;
-        for(char ngbr:adj.getOrDefault(curr_ch,new ArrayList<>())){
-            if(!vis[ngbr-'a']){
-                char temp=dfs(adj,ngbr,vis);
-                if(ans>temp){
-                ans=temp;
+    class union{
+        private int parent[];
+        public union(int n){
+            parent =new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
             }
-
-            }
-            
         }
-        return ans;
+        public int find(int x){
+            if(parent[x]==x){
+                return x;
+            }
+            return find(parent[x]);
+        }
+        public void union(int x,int y){
+            int rootX=find(x);
+            int rootY=find(y);
+            if(rootX!=rootY){
+                if(rootX<rootY){
+                    parent[rootY]=rootX;
+                }
+                else{
+                    parent[rootX]=rootY;
+                }
+            }
+        }
     }
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        int n=s1.length();
-        HashMap<Character,List<Character>>adj=new HashMap<>();
-        for(int i=0;i<n;i++){
-            char u=s1.charAt(i);
-            char v=s2.charAt(i);
-            //creating a undirected graph
-            adj.putIfAbsent(u,new ArrayList<>());
-            adj.putIfAbsent(v,new ArrayList<>());
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+        union union=new union(26);
+        for(int i=0;i<s1.length();i++){
+            char ch1=s1.charAt(i);
+            char ch2=s2.charAt(i);
+            union.union(ch1-'a',ch2-'a');
         }
         StringBuilder result=new StringBuilder();
         for(int i=0;i<baseStr.length();i++){
-            boolean vis[]=new boolean[26];
-            //dfs will find the min charater for that current character
-            char temp=dfs(adj,baseStr.charAt(i),vis);
-            result.append(temp);
+            char ch=baseStr.charAt(i);
+            result.append((char)(union.find(ch-'a')+'a'));
         }
         return result.toString();
     }
