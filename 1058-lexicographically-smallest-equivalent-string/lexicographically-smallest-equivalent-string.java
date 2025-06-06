218dbe852c447f7c864f3f41b8d1ad5685d27e77
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-06-06 12:09:34 +0530

    Time: 2 ms (96.88%) | Memory: 41.9 MB (78.18%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
@@ -1,8 +1,12 @@
+
+//solve using dsu and also basic create a parent array 
+// make a parent array of itself 
+//create a object then remember that in which we need small character so that we store parent smaller
 class Solution {
-    class union{
+    class Union{
         private int parent[];
-        public union(int n){
-            parent =new int[n];
+        public Union(int n){
+            parent=new int[n];
             for(int i=0;i<n;i++){
                 parent[i]=i;
             }
@@ -27,16 +31,16 @@ class Solution {
         }
     }
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-        union union=new union(26);
+        Union union=new Union(26);
         for(int i=0;i<s1.length();i++){
             char ch1=s1.charAt(i);
             char ch2=s2.charAt(i);
             union.union(ch1-'a',ch2-'a');
         }
-        StringBuilder result=new StringBuilder();
+        StringBuilder  result=new StringBuilder();
         for(int i=0;i<baseStr.length();i++){
             char ch=baseStr.charAt(i);
-            result.append((char)(union.find(ch-'a')+'a'));
+            result.append((char)((union.find(ch-'a'))+'a'));
         }
         return result.toString();
     }
