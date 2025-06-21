82b87d0e3a57c48bb49fa5dfbf86aea4d04a2fb0
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-06-22 01:09:50 +0530

    Time: 5 ms (94.04%) | Memory: 46.2 MB (9.27%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
@@ -1,3 +1,8 @@
+//for optimization we can sort the freq
+// already know that prev are already smaller so using cum_sum add in delete
+// so we also start from ending in j loop till we get no delete need we break at that point
+
+
 class Solution {
     public int minimumDeletions(String word, int k) {
         int freq[]=new int [26];
@@ -6,19 +11,48 @@ class Solution {
             int ch=word.charAt(i)-'a';
             freq[ch]++;
         }
+        Arrays.sort(freq);
         int ans=Integer.MAX_VALUE;
+        int sum=0;
         for(int i=0;i<26;i++){
-            int delete=0;
-            for(int j=0;j<26;j++){
-                if(freq[j]<freq[i]){
-                    delete+=freq[j];
+            int delete=sum;
+            for(int j=25;j>i;j--){
+                
+                if(freq[j]-freq[i]<=k){
+                   break;
                 }
-                else if(Math.abs(freq[j]-freq[i])>k){
                     delete+=Math.abs(freq[j]-freq[i]-k);
-                }
             }
             ans=Math.min(delete,ans);
+            sum+=freq[i];
         }
         return ans;
     }
 }
+
+
+
+// class Solution {
+//     public int minimumDeletions(String word, int k) {
+//         int freq[]=new int [26];
+//         int n=word.length();
+//         for(int i=0;i<n;i++){
+//             int ch=word.charAt(i)-'a';
+//             freq[ch]++;
+//         }
+//         int ans=Integer.MAX_VALUE;
+//         for(int i=0;i<26;i++){
+//             int delete=0;
+//             for(int j=0;j<26;j++){
+//                 if(freq[j]<freq[i]){
+//                     delete+=freq[j];
+//                 }
+//                 else if(Math.abs(freq[j]-freq[i])>k){
+//                     delete+=Math.abs(freq[j]-freq[i]-k);
+//                 }
+//             }
+//             ans=Math.min(delete,ans);
+//         }
+//         return ans;
+//     }
+// }
