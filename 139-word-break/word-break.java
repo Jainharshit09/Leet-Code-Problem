cc55202c36616b6d259d6a53197aa89d68a2e948
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-06-24 02:43:00 +0530

    Time: 2 ms (86.52%) | Memory: 42.7 MB (71.49%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,16 +1,71 @@
-// in which we check the till last index and string is valid or not
-// means for that we do idx-end;
+// for optimzation we can use trie stor word wordDict
+class Trie{
+    Trie ch[];
+    boolean isEnd;
+    public Trie(){
+        ch=new Trie[26];
+        for(int i=0;i<26;i++){
+            ch[i]=null;
+        }
+        isEnd=false;
+    }
+    public void insert(Trie root,String s){
+        Trie curr=root;
+        for(int i=0;i<s.length();i++){
+            int idx=s.charAt(i)-'a';
+            if(curr.ch[idx]==null){
+                curr.ch[idx]=new Trie();
+            }
+            curr=curr.ch[idx];
+        }
+        curr.isEnd=true;
+    }
+    public boolean search(Trie root,String str){
+        Trie curr=root;
+        for(int i=0;i<str.length();i++){
+            int index=str.charAt(i)-'a';
+            if(curr.ch[index]==null){
+                return false;
+            }
+            curr=curr.ch[index];
+        }
+        if(curr!=null && curr.isEnd==true)
+            return true;
+
+        return false;
+    }
+}
 class Solution {
-    public boolean wordBreak(String s, List<String> wordDict) {
-        boolean dp[]=new boolean[s.length()+1];
-        dp[0]=true;
-        Set<String> wordSet = new HashSet<>();
-        wordSet.addAll(wordDict);
-        for(int idx=1;idx<dp.length;idx++){
-            for(int end=1;end<=idx;end++){
-                dp[idx]=dp[idx]||(dp[idx-end] && wordSet.contains(s.substring(idx-end,idx)));
+    int n;
+    int dp[];
+    public boolean solve(int idx,String s,Trie root){
+        if(idx>=n)return true;
+        if(dp[idx]!=-1){
+            return dp[idx]==0?false:true;
+        }
+        Trie curr=root;
+        for(int i=idx;i<n;i++){
+            int index=s.charAt(i)-'a';
+            if(curr.ch[index]==null)break;
+            curr=curr.ch[index];
+            if(curr.isEnd){
+                if(solve(i+1,s,root)){
+                    dp[idx]=1;
+                    return true;
+                }
             }
         }
-        return dp[s.length()];
+        dp[idx]=0;
+        return false;
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Trie root=new Trie();
+        n=s.length();
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        for(String res:wordDict){
+            root.insert(root,res);
+        }
+        return solve(0,s,root);
     }
 }
