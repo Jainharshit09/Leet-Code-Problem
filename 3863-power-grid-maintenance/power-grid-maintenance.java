4e5e880eb3fa0016716190d6324d42665d3ebad7
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-07-06 12:16:33 +0530

    Time: 134 ms (54.22%) | Memory: 166.3 MB (25.81%) - LeetSync

---
+++ 3863-power-grid-maintenance/power-grid-maintenance.java
@@ -0,0 +1,71 @@
+//this question toward the connected components
+// make sure in tht hashmap integer,Treeset we can use
+// after that set will be in sorted order
+// create a ans of c+1 and check that if index is true directly add in ans
+// otherwise traverse from starting and make which one is true
+// if query start with 2 make false
+class DSU{
+    int parent[];
+    public DSU(int n){
+        parent=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    public int find(int x){
+        if(parent[x]!=x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    public void union(int x,int y){
+        int new_x=find(x);
+        int new_y=find(y);
+        if(new_x!=new_y){
+            parent[new_y]=new_x;
+        }
+    }
+}
+class Solution {
+    public int[] processQueries(int c, int[][] connections, int[][] queries) {
+        HashMap<Integer,TreeSet<Integer>>adj=new HashMap<>();
+        boolean check[]=new boolean[c+1];
+        Arrays.fill(check,true);
+        DSU root=new DSU(c+1);
+        for(int edge[]:connections){
+            root.union(edge[0],edge[1]);
+        }
+        for(int i=1;i<=c;i++){
+            int comp=root.find(i);
+            adj.putIfAbsent(comp, new TreeSet<>());
+            adj.get(comp).add(i);
+        }
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int i=0;i<queries.length;i++){
+            int type=queries[i][0];
+            int node=queries[i][1];
+            if(type==1){
+                if(check[node]==true)list.add(node);
+                else{
+                    //finding parent of that node and smallest one from that
+                    int p=root.find(node);
+                    TreeSet<Integer>set=adj.get(p);
+                    list.add(set.isEmpty()? -1 : set.first());
+                }
+            }
+            else{
+                if(check[node]==true){
+                    check[node]=false;
+                    int p=root.find(node);
+                    adj.get(p).remove(node);
+                }
+
+            }
+        }
+        int ans[]=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            ans[i]=list.get(i);
+        }
+        return ans;
+    }
+}
