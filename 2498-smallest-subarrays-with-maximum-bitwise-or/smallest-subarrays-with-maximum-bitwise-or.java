1ade8e95138910ed47f54e20b8d8cb00b683b0dd
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-07-30 01:13:25 +0530

    Time: 25 ms (85.53%) | Memory: 61.5 MB (65.79%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.java
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.java
@@ -1,57 +1,24 @@
-// Find max_or from index i to n-1 using reverse traversal.
-// Use sliding window: i = left, j = right, currOr = 0
-// Maintain bitCount[32] to track how many times each bit is present in window.
-// While j < n and currOr != max_or[i]:
-// For each bit b in nums[j]:
-// If bit is set, bitCount[b]++, and currOr |= (1 << b)
-// Move j++
-// When currOr == max_or[i], store ans[i] = j - i
-// Before moving i++, remove nums[i]:
-// For each bit b in nums[i]:
-// If bit is set, bitCount[b]--
-// If bitCount[b] == 0, then currOr &= ~(1 << b)
-// Repeat till i == n
-
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        int[] maxOr = new int[n];
-        // Step 1: Precompute maxOr from right to left
-        int or = 0;
+        int[] result = new int[n];
+        int[] setBitIndex = new int[31];
+        Arrays.fill(setBitIndex, -1);
+
         for (int i = n - 1; i >= 0; i--) {
-            or |= nums[i];
-            maxOr[i] = or;
-        }
-        int[] bitCount = new int[32];
-        int currOr = 0;
-        int j = 0;
-        int i=0;
-        while(i<n) {
-            // Move j forward until currOr == maxOr[i]
-            while (j < n && currOr != maxOr[i]) {
-                int val = nums[j];
-                for (int b = 0; b < 32; b++) {
-                    if (((val >> b) & 1) == 1) {
-                        bitCount[b]++;
-                        currOr |= (1 << b);
+            int endIndex = i;
+            for (int j = 0; j < 31; j++) {
+                if ((nums[i] & (1 << j)) == 0) {
+                    if (setBitIndex[j] != -1) {
+                        endIndex = Math.max(endIndex, setBitIndex[j]);
                     }
-                }
-                j++;
-            }
-              ans[i] = Math.max(1, j - i);
-            // Remove nums[i]'s bits from currOr
-            int val = nums[i];
-            for (int b = 0; b < 32; b++) {
-                if (((val >> b) & 1) == 1) {
-                    bitCount[b]--;
-                    if (bitCount[b] == 0) {
-                        currOr &= ~(1 << b); // clear bit b
-                    }
+                } else {
+                    setBitIndex[j] = i;
                 }
             }
-            i++;
+            result[i] = endIndex - i + 1;
         }
-        return ans;
+
+        return result;
     }
 }
