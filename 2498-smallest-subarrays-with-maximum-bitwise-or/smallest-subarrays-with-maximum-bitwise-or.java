9de1281b14e58a73168802d725dddf95b4a29c79
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-07-30 01:12:32 +0530

    Time: 29 ms (39.47%) | Memory: 63.4 MB (13.16%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.java
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.java
@@ -1,3 +1,17 @@
+// Find max_or from index i to n-1 using reverse traversal.
+// Use sliding window: i = left, j = right, currOr = 0
+// Maintain bitCount[32] to track how many times each bit is present in window.
+// While j < n and currOr != max_or[i]:
+// For each bit b in nums[j]:
+// If bit is set, bitCount[b]++, and currOr |= (1 << b)
+// Move j++
+// When currOr == max_or[i], store ans[i] = j - i
+// Before moving i++, remove nums[i]:
+// For each bit b in nums[i]:
+// If bit is set, bitCount[b]--
+// If bitCount[b] == 0, then currOr &= ~(1 << b)
+// Repeat till i == n
+
 class Solution {
     public int[] smallestSubarrays(int[] nums) {
         int n = nums.length;
@@ -9,13 +23,11 @@ class Solution {
             or |= nums[i];
             maxOr[i] = or;
         }
-
-        int[] bitCount = new int[32];  // To track each bit's frequency in window
+        int[] bitCount = new int[32];
         int currOr = 0;
         int j = 0;
-
-        for (int i = 0; i < n; i++) {
-
+        int i=0;
+        while(i<n) {
             // Move j forward until currOr == maxOr[i]
             while (j < n && currOr != maxOr[i]) {
                 int val = nums[j];
@@ -27,9 +39,7 @@ class Solution {
                 }
                 j++;
             }
-
               ans[i] = Math.max(1, j - i);
-
             // Remove nums[i]'s bits from currOr
             int val = nums[i];
             for (int b = 0; b < 32; b++) {
@@ -40,8 +50,8 @@ class Solution {
                     }
                 }
             }
+            i++;
         }
-
         return ans;
     }
 }
