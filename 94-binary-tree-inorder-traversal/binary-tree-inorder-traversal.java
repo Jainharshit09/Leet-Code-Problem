e53fb67f712971a9cd1c8848aad290aa6c7ec45b
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-08-04 23:41:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (27.13%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -13,19 +13,29 @@
  *     }
  * }
  */
+
+ //using morris inorder traversal we can do
+ // go left right and connect to curr
+ // than make sure remove that curr.left but before that connect with right to curr
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>ans=new ArrayList<>();
-        Stack<TreeNode>stack=new Stack<>();
         TreeNode curr=root;
-        while(curr!=null || !stack.isEmpty()){
-            while(curr!=null){
-                stack.push(curr);
-                curr=curr.left;
+        List<Integer>ans=new ArrayList<>();
+        while(curr!=null){
+            if(curr.left==null){
+                ans.add(curr.val);
+                curr=curr.right;
             }
-            curr=stack.pop();
-            ans.add(curr.val);
-            curr=curr.right;
+            else{
+                TreeNode leftchild=curr.left;
+                while(leftchild.right!=null){
+                    leftchild=leftchild.right;
+                }
+                leftchild.right=curr;
+                TreeNode temp=curr;
+                curr=curr.left;
+                temp.left=null;
+            }
         }
         return ans;
     }
