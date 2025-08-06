c8ae499aa715d0da594f2cc75ecd7acdf36bf6e8
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-08-06 16:30:00 +0530

    Time: 3 ms (19.61%) | Memory: 45 MB (9.76%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,21 +1,51 @@
+//using segment tree we can solve;
+
+
 class Solution {
+    public void buildTree(long seg[],int i,int l,int r, int arr[]){
+        if(l==r){
+            seg[i]=arr[l];
+            return;
+        }
+        int mid=l+(r-l)/2;
+        buildTree(seg,2*i+1,l,mid,arr);
+        buildTree(seg,2*i+2,mid+1,r,arr);
+        seg[i]=Math.max(seg[2*i+1],seg[2*i+2]);
+    }
+    public int query(long seg[],int i,int l,int r,int val){
+        if(seg[i]<val)return -1;
+        if(l==r)return l;
+        int mid=l+(r-l)/2;
+        //go left first than check ans is possible than return from there if no than go to right
+        int left=query(seg,2*i+1,l,mid,val);
+        if(left!=-1)return left;
+        return query(seg,2*i+2,mid+1,r,val);
+    }
+    public void update(long seg[],int i,int l,int r,int index){
+        if(l==r){
+            seg[i]=0;
+            return;
+        }
+         int mid=l+(r-l)/2;
+         if(index<=mid){
+            update(seg,2*i+1,l,mid,index);
+         }
+         else{
+            update(seg,2*i+2,mid+1,r,index);
+         }
+         seg[i]=Math.max(seg[2*i+1],seg[2*i+2]);
+    }
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        int n=fruits.length;
-        int m =baskets.length;
+        int n=baskets.length;
         int unplaced=0;
-        boolean vis[]=new boolean[m];
-        for(int i=0;i<n;i++){
-            int curr=fruits[i];
-            boolean check=false;
-            for(int j=0;j<m;j++){
-                int take=baskets[j];
-                if(curr<=take && vis[j]==false){
-                    vis[j]=true;
-                    check=true;
-                    break;
-                }
+        long seg[]=new long[4*n];
+        buildTree(seg,0,0,n-1,baskets);
+        for(int fruit:fruits){
+            int queryIndex=query(seg,0,0,n-1,fruit);
+            if(queryIndex!=-1){
+                update(seg,0,0,n-1,queryIndex);
             }
-            if(check==false){
+            else{
                 unplaced++;
             }
         }
