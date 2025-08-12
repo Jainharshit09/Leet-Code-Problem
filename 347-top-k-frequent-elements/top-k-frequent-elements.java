ad84b74faf0d274747e6588f15c9b4200d83b133
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-08-12 15:43:48 +0530

    Time: 14 ms (57.65%) | Memory: 49 MB (34.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,22 +1,24 @@
+//use hashmap to store the count
+//after that take element having maximun count of top k will be added in pq and tracverse the pq and get the ans;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i], 0) + 1);
-        }
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
-            (a, b) -> map.get(a) - map.get(b)
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }  
+        PriorityQueue<Integer>pq=new PriorityQueue<>(
+             (a, b)-> map.get(a)-map.get(b)
         );
         for(int num:map.keySet()){
-              minHeap.offer(num);
-              if(minHeap.size()>k){
-                minHeap.poll();
-              }
+            pq.add(num);
+            if(pq.size()>k)pq.poll();
         }
-          int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = minHeap.poll();
+        int ans[]=new int [k];
+        for(int i=0;i<k;i++){
+            ans[i]=pq.poll();
         }
-        return result;
+        return ans;
     }
 }
