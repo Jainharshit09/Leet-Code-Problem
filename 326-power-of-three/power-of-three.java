e66524842ad38daf663ac9dd938b781e0e527683
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-08-13 14:43:34 +0530

    Time: 7 ms (100.00%) | Memory: 44.2 MB (51.08%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,9 +1,6 @@
 class Solution {
-    public double log3(int n){
-        return  Math.log10(n) / Math.log10(3);
-    }
     public boolean isPowerOfThree(int n) {
         if(n<=0)return false;
-        return log3(n)==(int)(log3(n));
+        return 1162261467%n==0;
     }
 }
