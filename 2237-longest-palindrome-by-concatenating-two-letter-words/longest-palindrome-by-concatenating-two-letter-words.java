491e594418eb0afde443b132fbce9b32e5f4c9e3
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-08-17 20:24:54 +0530

    Time: 128 ms (19.32%) | Memory: 57.9 MB (65.66%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,22 +1,24 @@
+//i think in which trie will be used because like string is lt in trie tl is there means we take it  if 
+//or we can use hashmap also like in string store and if check there reverse that present than increase the count+=4 and decrase it value;
+//now for same character if both 1 and second 2 means count+=2 and break only one time we can add
 class Solution {
     public int longestPalindrome(String[] words) {
         HashMap<String,Integer>map=new HashMap<>();
-        int n=words.length;
         int count=0;
         for(String word:words){
-              String reversed = new StringBuilder(word).reverse().toString();
-              if(map.getOrDefault(reversed, 0) > 0){
+            String reversed=new StringBuilder(word).reverse().toString();
+            if(map.containsKey(reversed)){
                 count+=4;
                 map.put(reversed,map.getOrDefault(reversed,0)-1);
-              }
-              else{
+                if(map.get(reversed)<=0) map.remove(reversed);
+            }
+            else{
                 map.put(word,map.getOrDefault(word,0)+1);
-              }
+            }
         }
-
-        for(Map.Entry<String,Integer>entry:map.entrySet()){
-             String word = entry.getKey();
-            int result = entry.getValue();
+        for(Map.Entry<String,Integer> e:map.entrySet()){
+            String word=e.getKey();
+            int result = e.getValue();
             if(word.charAt(0)==word.charAt(1) && result>0){
                 count+=2;
                 break;
