c469a211f42b21111fc83c0df30561cc4c2376eb
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2025-08-18 19:41:39 +0530

    Time: 8 ms (95.68%) | Memory: 44.8 MB (79.80%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -1,29 +1,39 @@
-//like we can go in circluar motion from in clock wise
-// we can solve using queue store both in different queue
-// after that which one is next we remove that and add again in it which one is not delete we add in q in end any q will be empty return it oppisite will win
-
 class Solution {
     public String predictPartyVictory(String senate) {
-        int n=senate.length();
-        Queue<Integer>R_count=new LinkedList<>();
-        Queue<Integer>D_count=new LinkedList<>();
-        for(int i=0;i<n;i++){
-            char ch=senate.charAt(i);
-            if(ch=='R')R_count.add(i);
-            else{
-                D_count.add(i);
+        Queue<Character> queue = new LinkedList<>();
+        int rCount = 0, dCount = 0; // count of active senators
+        int rBan = 0, dBan = 0;     // pending bans against each party
+
+        // Initial processing
+        for (char c : senate.toCharArray()) {
+            queue.offer(c);
+            if (c == 'R') rCount++;
+            else dCount++;
+        }
+
+        // Keep going until one party is eliminated
+        while (rCount > 0 && dCount > 0) {
+            char curr = queue.poll();
+
+            if (curr == 'R') {
+                if (rBan > 0) {
+                    rBan--; // this R is banned
+                    rCount--;
+                } else {
+                    dBan++; // R bans a D
+                    queue.offer('R'); // R survives, goes to end of queue
+                }
+            } else {
+                if (dBan > 0) {
+                    dBan--; // this D is banned
+                    dCount--;
+                } else {
+                    rBan++; // D bans an R
+                    queue.offer('D'); // D survives
+                }
             }
         }
-        while(!R_count.isEmpty() && !D_count.isEmpty()){
-            int r_pos=R_count.poll();
-            int d_pos=D_count.poll();
-            if(r_pos<d_pos){
-                R_count.add(r_pos+n);
-            }
-            else{
-                D_count.add(d_pos+n);
-            }
-        }
-        return R_count.isEmpty()?"Dire":"Radiant";
+
+        return rCount > 0 ? "Radiant" : "Dire";
     }
 }
